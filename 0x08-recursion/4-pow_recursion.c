5c73348f9b13f2c177a15420cff714027f85692f
Jacob Stephen <cashflow34steph@gmail.com>
2025-02-15 11:24:49 -0800

    returns the power of x to y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - prints the power of y to x
+ * @x: parameter
+ * @y: parameter
+ *
+ * Return: returns the poer of x to y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return 1;
+	return(x *_pow_recursion(x, y - 1));
+}
