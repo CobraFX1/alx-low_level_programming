0b3e96d073ac14942672a85a9050500cfc56f7d6
Jacob Stephen <cashflow34steph@gmail.com>
2025-02-23 07:01:18 -0800

    Printing the number of arguments thats passed across

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - prints the number of count
+ * @argc: counts the number of arguments
+ * @argv: an array for the arguments inputted
+ *
+ * Return: 0(Success)
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
