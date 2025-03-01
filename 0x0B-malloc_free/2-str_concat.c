00302ffd1d34be4802c4e28969c5f6c7e91f5466
Jacob Stephen <cashflow34steph@gmail.com>
2025-03-01 07:34:38 -0800

    concatenates two strings and returns a pointer to the result

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,7 +17,7 @@ char *str_concat(char *s1, char *s2)
 	int counts2 = 0;
 	int count = 0;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
 	while (s1[i] != '\0')
 	{
