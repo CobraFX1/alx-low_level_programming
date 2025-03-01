5f68760ddda90286ca8912e404505c09711c1f3b
Jacob Stephen <cashflow34steph@gmail.com>
2025-03-01 05:56:43 -0800

    duplicates a string and returns the pointer to the new string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strdup - creates a copy of a string and return a pointer to it
+ * @str: the string to duplicate
+ *
+ * Return: pointer to the new string
+ */
+
+char *_strdup(char *str)
+{
+	char *new;
+	int count = 0;
+	int i = 0;
+	int j;
+
+	while (str[i] != '\0')
+	{
+		count += 1;
+		i++;
+	}
+	new = malloc(sizeof(char) * count);
+	if (new == NULL)
+		return (NULL);
+	for (j = 0; j < count; j++)
+	{
+		new[j] = str[j];
+	}
+	return (new);
+}
