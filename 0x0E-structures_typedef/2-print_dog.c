a915836c78b36be321eb1f868a9e8fbff69a8848
Jacob Stephen <cashflow34steph@gmail.com>
2025-03-17 03:10:12 -0700

    prints out information on the variable struct dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog -  prints out the values in struct dog
+ * @d: the pointer to the variable
+ *
+ * Return: nothing
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		printf("Name: %s\n", "(nil)");
+	else
+		printf("NAME: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	if (d->owner == NULL)
+		printf("Owner: %s\n", "(nil)");
+	else
+		printf("Owner: %s\n", d->owner);
+}
