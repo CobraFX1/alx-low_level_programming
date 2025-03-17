1e1d97a0e9426e44f9e7e9f66b8c09b6f3ba6a98
Jacob Stephen <cashflow34steph@gmail.com>
2025-03-17 02:35:20 -0700

    defines the new datatype struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - defines the structure of the datatyoe struct dog
+ * @name: member
+ * @age: member
+ * @owner: member
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
