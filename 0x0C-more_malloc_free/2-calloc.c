1248ab8270f402b516876511974f3fd9d24cbf23
Jacob Stephen <cashflow34steph@gmail.com>
2025-03-16 22:34:50 -0700

    Assigns a number of elements with size

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _calloc - assigns memory dynamically
+ * @nmemb: number of elements
+ * @size: size of each element
+ *
+ * Return: pointer to the new memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *new;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	new = malloc(size * nmemb);
+	if (new == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	for (i = 0; i < nmemb; i++)
+		new[i] = 0;
+	return (new);
+}
