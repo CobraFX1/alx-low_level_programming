79fd272296461b0f9ca394a7aeb4aff014f17aeb
Jacob Stephen <cashflow34steph@gmail.com>
2025-03-16 23:29:08 -0700

    dynamically allocated memory

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,18 +9,19 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *new;
+	void *new;
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	new = (char *)malloc(size * nmemb);
+	new = malloc(size * nmemb);
 	if (new == NULL)
 	{
 		free(new);
 		return (NULL);
 	}
-	for (i = 0; i < nmemb; i++)
-		new[i * size] = 0;
+	unsigned char *byte_new = (unsigned char *)new;
+	for (i = 0; i < size * nmemb; i++)
+		byte_new[i] = 0;
 	return (new);
 }
