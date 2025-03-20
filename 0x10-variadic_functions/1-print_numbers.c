87705550aec3493e4ad66f6123deecb2a2a7ab2e
Jacob Stephen <cashflow34steph@gmail.com>
2025-03-20 07:29:04 -0700

    prints a set of lines

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,12 +13,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list args;
 
-	if (n == 0)
-		return;
 	va_start(args, n);
-	printf("%d", va_arg(args, int));
 	for (i = 1; i < n; i++)
 	{
+		printf("%d", va_arg(args, int));
 		if (separator != NULL)
 		{
 			printf("%s", separator);
