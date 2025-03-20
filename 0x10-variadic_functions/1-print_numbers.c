218b5e521a34e697b4f9e5cb2a6269b0e659c448
Jacob Stephen <cashflow34steph@gmail.com>
2025-03-20 07:11:34 -0700

    prints a set if numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include "stdarg.h"
+
+/**
+ * print_numbers - prints out a set of numbers with a seperator
+ * @separator: parameter
+ * @n: parameter
+ *
+ * Return: nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+
+	va_start(args, n);
+	printf("%d", va_arg(args, int));
+	for (i = 1; i < n; i++)
+	{
+		if (separator != NULL)
+		{
+			printf("%s", separator);
+			printf("%d", va_arg(args, int));
+		}
+		else
+			printf("%d", va_arg(args, int));
+	}
+	printf("\n");
+}
