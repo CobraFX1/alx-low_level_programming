2e769ea6954e975adefee4f5f1a81eafb2642d03
Jacob Stephen <cashflow34steph@gmail.com>
2025-04-15 04:46:00 -0700

    printing multiple characters

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,41 +8,41 @@
  */
 void print_all(const char * const format, ...)
 {
-    va_list args;
-    int i = 0;
-    char *str;
-    char *sep = "";
+	va_list args;
+	int i = 0;
+	char *str;
+	char *sep = "";
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (format && format[i])
-    {
-        switch (format[i])
-        {
-        case 'c':
-            printf("%s%c", sep, va_arg(args, int));
-            sep = ", ";
-            break;
-        case 'i':
-            printf("%s%d", sep, va_arg(args, int));
-            sep = ", ";
-            break;
-        case 'f':
-            printf("%s%f", sep, va_arg(args, double));
-            sep = ", ";
-            break;
-        case 's':
-            str = va_arg(args, char *);
-            /* Only one if here to check if the string is NULL */
-            if (str == NULL)
-                str = "(nil)";
-            printf("%s%s", sep, str);
-            sep = ", ";
-            break;
-        }
-        i++;
-    }
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%s%c", sep, va_arg(args, int));
+				sep = ", ";
+				break;
+			case 'i':
+				printf("%s%d", sep, va_arg(args, int));
+				sep = ", ";
+				break;
+			case 'f':
+				printf("%s%f", sep, va_arg(args, double));
+				sep = ", ";
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				/* Only one if here to check if the string is NULL */
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s%s", sep, str);
+				sep = ", ";
+				break;
+		}
+		i++;
+	}
 
-    va_end(args);
-    printf("\n");
+	va_end(args);
+	printf("\n");
 }
