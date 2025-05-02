e4c4b81016272f8ce4b02c1239e90412603054ef
Jacob Stephen <cashflow34steph@gmail.com>
2025-05-02 04:12:07 -0700

    prints the number of elements

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -16,5 +16,6 @@ typedef struct listint_s
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+size_t listint_len(const listint_t *h);
 size_t print_listint(const listint_t *h);
 #endif /* LISTS_H */
